9e9f33157362ad55b112d18a7b9313b49ac431ee
nishantraj21 <nishantraj21092004@gmail.com>
2026-04-18 16:01:02 +0530

    avoid losing data

---
+++ optionalchanning.js
@@ -0,0 +1,9 @@
+let x = {
+    name : "Nishant",
+    age : 22,
+    //city : "Delhi"
+}
+
+console.log(x.name);
+console.log(x.age);
+console.log(x.city?.address);
--- summary.js
+++ summary.js
@@ -1,2 +1,11 @@
-console.log("hello world");
+let userOne = {
+    email : "user@gmail.com",
+    upi : "user@ybl"
+}
+
+let userTwo = userOne
+userTwo.email = "nishant@gmail.com"
+
+console.log(userOne);
+console.log(userTwo);
 
