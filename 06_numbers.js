b68474f3e2e06357050419fbbf227879d3f656c2
nishantraj21 <nishantraj21092004@gmail.com>
2026-04-26 08:40:00 +0530

    added some new file

--- 06_numbers.js
+++ 06_numbers.js
@@ -3,7 +3,7 @@ let b = 345.64875;
 console.log(b.toFixed(1)); // number will show upto 1 decimal values.
 console.log(b.toPrecision(5)); // number will show upto 5 digits.
 
-console.log(typeof b.toFixed(1));
+console.log(typeof b.toFixed(1));//string
 let m = new Number(30);
 console.log(typeof m); //object
 let n = new Number(30);
--- 11_object.js
+++ 11_object.js
@@ -1,7 +1,9 @@
 const user = {
     name: "Nishant",
     age: 22,
-    emailId: "nishant@gmail.com" 
+    emailId: "nishant@gmail.com", 
+    0: 100,
+    1: 200,
 }
 
 console.log(user);
@@ -17,3 +19,58 @@ console.log(Object.keys(user));
 console.log(Object.values(user));
 
 console.log(Object.entries(user));
+
+// another method to access key and value
+for(let keys in user ){
+    console.log(keys, user[keys]);
+    
+}
+
+//destructuring the object
+
+const {name,age} = user;
+
+console.log(name,age);
+
+const arr = [10,20,30,40,50];
+
+const [first,second] = arr;
+console.log(first,second);
+
+const {emailId: userEmail, aadhar: userAadhar} = user
+console.log(userEmail, userAadhar);
+
+
+// function in object
+
+const calculator = 
+{
+    input1: 25,
+    input2: 47,
+    name: "calc",
+    //greeting: function(){
+    //    console.log(`welcome! on this ${this.name}`);
+    //    return 2026;
+    //},
+    address: {
+        city: "patna",
+        state: "Bihar"
+    } 
+
+}
+
+//console.log(calculator.greeting());
+console.log(calculator.address.city);
+
+// shallow copy
+// const user2 = {...user}; // Indipendent copy but it will not correct nested object
+
+//deep copy 
+// let temp = {};
+// Object.assign(temp, calculator);
+// console.log(temp);
+
+const user2 = structuredClone(calculator);
+user2.address.city = "Aurangabad";
+console.log(calculator); // in deep clone dont clone functions and DOM elements.
+console.log(user2);
