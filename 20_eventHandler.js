0aa0087724a96046824e0b49cdca02e780401997
nishantraj21 <nishantraj21092004@gmail.com>
2026-06-18 11:17:04 +0530

    added new files

--- 20_eventHandler.js
+++ 20_eventHandler.js
@@ -6,3 +6,46 @@ function handleClick(){
     const element = document.getElementById("first");
     element.textContent = "Welecome to my Website!";
 }
+
+// another method --> without function call in html file
+
+const element2 = document.getElementById("second");
+element2.onclick = function handleChanges(){
+    element2.textContent = "Have a Nice day...";
+} // if we call again then it will override so this is not good method 
+
+// Best Method ***************************************
+const element3 = document.getElementById("third");
+element3.addEventListener('click', ()=>{
+    element3.textContent = "Its Working....."
+})
+
+element3.addEventListener('click', ()=>{
+    element3.style.backgroundColor="green";
+    element3.style.color="white";
+})
+
+const parent = document.getElementById("parent");
+console.log(parent.children);
+
+
+for (let child of parent.children){
+    child.addEventListener('click',()=>{
+        child.textContent = "I am clicked"
+    })
+}
+
+const grandParent = document.getElementById("grandparent");
+grandParent.addEventListener('click',()=>{
+    console.log("Grandparent is Clicked");
+})
+
+const Parent = document.getElementById("parent2");
+Parent.addEventListener('click',()=>{
+    console.log("Parent is Clicked");
+})
+
+const child = document.getElementById("child6");
+child.addEventListener('click',()=>{
+    console.log("Child is Clicked");
+})
